9ff1cec4e3fbc207f0109180196dfb5d93c3cf6b
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-10-02 17:23:19 +0000

    Static method in java

---
+++ Methods/staticMethod.java
@@ -0,0 +1,36 @@
+/*
+Static Methods in Java
+*/
+
+public class staticMethod
+{
+    public static void main(String args[])
+    {
+        //To call the staic method of same class then no need to create object
+        m1();
+        //To call the static method of another class then use the classname of it
+        Demo.m2();
+    }
+
+    //Static method m1 defination
+    public static void m1()
+    {
+        System.out.println("Method1");
+    }
+}
+
+//Another class Demo
+class Demo
+{
+    //static method of another class m2 defination
+    public static void m2()
+    {
+        System.out.println("Method2");
+    }
+}
+
+
+/*output
+Method1
+Method2
+*/

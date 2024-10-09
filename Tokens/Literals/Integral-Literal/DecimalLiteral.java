1098139fbb73bea1f486a4a734d78d4fc8329e6a
Yash-6503 <yashwalke6503@gmail.com>
2024-10-09 23:38:59 +0530

    Decimal Literals in Integral-Literals

---
+++ Tokens/Literals/Integral-Literal/DecimalLiteral.java
@@ -0,0 +1,28 @@
+/*
+By default our numeric literals are decimal literal. 
+Here base is 10 so, It accepts 10 digits i.e. from 0-9.
+*/
+
+package com.classes;
+
+public class DecimalLiteral 
+{
+    public static void main(String[] args)
+    {
+        int i = 10; // Decimal Literal
+        int j = 210; // Decimal Literal
+        int k = 327678; // Decimal Literal
+        int a = 0; // Decimal Literal
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(a);
+    }
+}
+
+/*
+10
+210
+327678
+0
+*/

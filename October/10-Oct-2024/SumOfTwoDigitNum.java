7b649adfeb6abeb304be8bfccaa4f013f021d15f
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-10-13 15:19:41 +0000

    Program to calculate sum of Two digit number also those number not belongs to 2 digits give them appropriate message

--- October/10-Oct-2024/SumOfTwoDigitNum.java
+++ October/10-Oct-2024/SumOfTwoDigitNum.java
@@ -9,10 +9,44 @@ if the given value is greater than 99, print -2
 if the given value is in between 0 and 9, print -1
 */
 
+package com.classes;
+
 public class SumOfTwoDigitNum
 {
     public static void main(String args[])
     {
-        System.out.println();
+        int num = Integer.parseInt(args[0]);
+
+        if(num > 9 && num < 100)
+        {
+            int sum = (num % 10) + (num / 10);
+            System.out.println("Sum of Two digit number "+num+" is "+sum);
+        }
+        else if(num < 0)
+        {
+            System.out.println("-3");
+        }
+        else if(num > 99)
+        {
+            System.out.println("-2");
+        }
+        else if(num >= 0 && num <= 9)
+        {
+            System.out.println("-1");
+        }
     }
 }
+
+/*
+java SumOfTwoDigitNum.java 34
+Sum of Two digit number 34 is 7
+
+java SumOfTwoDigitNum.java -34
+-3
+
+java SumOfTwoDigitNum.java 101
+-2
+
+java SumOfTwoDigitNum.java 5
+-1
+*/

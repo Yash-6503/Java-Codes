9f7a417a59b2583f5c34e8d0d3e9f90b4a0d650b
Yash-6503 <yashwalke6503@gmail.com>
2024-10-13 20:32:13 +0530

    Sum of two digit number

---
+++ October/10-Oct-2024/SumOfTwoDigitNum.java
@@ -0,0 +1,18 @@
+/*
+Que-1)
+Write a java which program the sum of digits of the given two digit number.
+program determined by the following rules:
+
+if the given value is in between 10 and 99, return sum of it's digits. Example: if x = 34, print 7
+if the given value is negative, print -3
+if the given value is greater than 99, print -2
+if the given value is in between 0 and 9, print -1
+*/
+
+public class SumOfTwoDigitNum
+{
+    public static void main(String args[])
+    {
+        System.out.println();
+    }
+}

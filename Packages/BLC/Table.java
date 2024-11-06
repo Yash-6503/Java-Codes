ca314dd4c3fe3eb73a76f3e84b31f914cedeae81
Yash-6503 <yashwalke6503@gmail.com>
2024-11-06 11:48:01 +0530

    Questions to solve

---
+++ Packages/BLC/Table.java
@@ -0,0 +1,12 @@
+package BLC;
+
+public class Table 
+{
+    public void getTable(int x)
+    {
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(i*x);
+        }
+    }
+}
---
+++ Packages/TableCalculator.java
@@ -0,0 +1,12 @@
+import BLC.Table;
+
+public class TableCalculator
+{
+    public static void main(String args[])
+    {
+        Table t1 = new Table();
+
+        t1.getTable(10);
+
+    }
+}

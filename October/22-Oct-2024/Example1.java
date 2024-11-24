34db4726418b781deab6b9ad226cddd9284330cb
Yash-6503 <yashwalke6503@gmail.com>
2024-11-24 12:58:12 +0530

    Program on OOPs to define Product class properties & behaviour using ELC & BLC classes

--- October/22-Oct-2024/Example1.java
+++ October/22-Oct-2024/Example1.java
@@ -1,17 +1,37 @@
 package com.blc;
 
+import java.util.Scanner;
+
 public class Example1
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Product Id: ");
+        int productId = sc.nextInt();
+
+        System.out.println("Enter the Product Name: ");
+        sc.next();
+        String productName = sc.nextLine();
+
+        System.out.println("Enter the Product Price: ");
+        double productPrice = sc.nextDouble();
+
         Product product1 = new Product();
-        product1.setProductData(101, "Laptop", 45000.00);
+        product1.setProductData(productId, productName, productPrice);
         product1.displayProductData();
     }
 }
 
 /*
+Enter the Product Id: 
+101
+Enter the Product Name: 
+Laptop
+Enter the Product Price: 
+50000
 Product ID: 101
-Product Name: Laptop
-Product Price: 45000.0
+Product Name: 
+Product Price: 50000.0
 */

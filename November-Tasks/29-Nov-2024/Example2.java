ab3c85f923672ef051133c0790841d3b7bca05fc
Yash-6503 <yashwalke6503@gmail.com>
2024-11-29 11:52:49 +0530

    Program based on Method Overloading to perform ShapeCalculations like area of square, rectangle & circle by using ELC & BLC classes

---
+++ November-Tasks/29-Nov-2024/Example2.java
@@ -0,0 +1,31 @@
+//ELC class -> main class
+
+package pkg.blc;
+
+public class Example2
+{
+    public static void main(String args[])
+    {
+        ShapeCalculator sc = new ShapeCalculator();
+
+        System.out.println(sc.calculateArea(-4) == 0 ? "" : "Area of Square = "+sc.calculateArea(4));
+
+        System.out.println(sc.calculateArea(-10,-10) == 0 ? "" : "Area of Rectangle = "+sc.calculateArea(10,10));
+        
+        System.out.println(sc.calculateArea(-7.0) == 0 ? "" : "Area of Circle = "+sc.calculateArea(7.0));
+    }
+}
+
+/*  Tested with positive values
+Area of Square = 16
+Area of Rectangle = 100
+Area of Circle = 153.86
+*/
+
+/*  Tested with negative values
+Side Length must be non-negative
+
+Length and width must be non-negative.
+
+Radius must be non-negative.
+*/
---
+++ November-Tasks/29-Nov-2024/pkg/blc/ShapeCalculator.java
@@ -0,0 +1,39 @@
+//BLC class of Example2 -> ShapeCalculator.java
+
+package pkg.blc;
+
+public class ShapeCalculator
+{
+    public int calculateArea(int sideLength){
+        if(sideLength <= 0){
+            System.out.println("Side Length must be non-negative");
+            return 0;
+        }else{
+            return sideLength * sideLength;
+        }
+    }
+
+    public int calculateArea(int length, int width){
+        if(length > 0 && width > 0){
+            return length * width;
+        }else if(length > 0 && width <= 0){
+            System.out.println("Width must be non-negative.");
+            return 0;
+        }else if(width > 0 && length <= 0){
+            System.out.println("Length must be non-negative.");
+            return 0;
+        }else{
+            System.out.println("Length and width must be non-negative.");
+            return 0;
+        }
+    }
+
+    public double calculateArea(double radius){
+        if(radius > 0){
+            return 3.14 * (radius * radius);
+        }else{
+            System.out.println("Radius must be non-negative.");
+            return 0;
+        }
+    }
+}

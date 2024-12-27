8a973f626344b0b4206775c4ebdab0bc5c3b58a4
Yash-6503 <yashwalke6503@gmail.com>
2024-12-27 21:59:50 +0530

    Program that demonstrates the usage of the Predicate interface to test if a number is even or prime.

--- December-Tasks/19-Dec-2024/Example1.java
+++ December-Tasks/19-Dec-2024/Example1.java
@@ -19,7 +19,27 @@ public class Example1
         System.out.println("Is "+num+" even? "+p1.test(num));
         
         Predicate<Integer> p2 = (Integer b) -> b%2==1;
-        System.out.println("Is "+num+" even? "+p2.test(num));
+        System.out.println("Is "+num+" prime? "+p2.test(num));
+
+        /* Optional code to find prime number
+         //To find number is prime or not
+        Predicate<Integer> prime = b -> {
+            int count = 0;
+            if(b == 1){
+                return true;
+            }
+            for(int i=1; i<=b; i++){
+                if(b % i == 0){
+                    count++;
+                }
+            }
+            if(count == 2){
+                return true;
+            }else{
+                return false;
+            }
+        };
+        */
     }
 }
 
@@ -27,19 +47,19 @@ public class Example1
 Enter a Number: 
 13
 Is 13 even? false
-Is 13 even? true
+Is 13 prime? true
 */
 
 /*
 Enter a Number: 
 5
 Is 5 even? false
-Is 5 even? true
+Is 5 prime? true
 */
 
 /*
 Enter a Number: 
 20
 Is 20 even? true
-Is 20 even? false
+Is 20 prime? false
 */

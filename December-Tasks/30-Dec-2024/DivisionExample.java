27e6271c7a702615b3a6a36fbca689def015ce55
Yash-6503 <yashwalke6503@gmail.com>
2024-12-30 11:29:28 +0530

    Program that demonstrates how to handle ArithmeticException.

--- December-Tasks/30-Dec-2024/DivisionExample.java
+++ December-Tasks/30-Dec-2024/DivisionExample.java
@@ -4,16 +4,26 @@ Program that demonstrates how to handle ArithmeticException.
 
 package com.classes;
 
+import java.util.Scanner;
+
 public class DivisionExample
 {
 	public static void main(String[] args) 
 	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter dividend : ");
+		int dividend = sc.nextInt();
+		
+		System.out.println("Enter divisor : ");
+		int divisor = sc.nextInt();
+		
 		try {
-			System.out.println(performDivision(12, 2));
-			System.out.println(performDivision(12, 0));
+			System.out.println("Result of division: "+performDivision(dividend,divisor));
 		}catch(Exception e) {
 			System.err.println(e);
 		}
+
 	}
 	
 	
@@ -25,6 +35,17 @@ public class DivisionExample
 }
 
 /*
-6 
+Enter dividend : 
+10
+Enter divisor : 
+2
+Result of division: 5
+*/
+
+/*
+Enter dividend : 
+5
+Enter divisor : 
+0
 java.lang.ArithmeticException: / by zero
 */

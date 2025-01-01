77eb373bc6548d2877a893365a95c83ce17d37f1
Yash-6503 <yashwalke6503@gmail.com>
2025-01-01 11:50:14 +0530

    Program that simulates various car-related exceptions using custom exception classes (CarStopped, CarPuncture, CarHeat).

---
+++ January-Tasks/1-Jan-2024/pkg/CarPunctureException.java
@@ -0,0 +1,11 @@
+////BLC class of CarUser -> CarPunctureException.java
+
+package pkg;
+
+public class CarPunctureException extends Exception
+{
+	public CarPunctureException(String puncture)
+	{
+		super(puncture);
+	}
+}
